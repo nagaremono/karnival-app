4e8e9161d2b90184804ada7e31d64b68dd495c9c
guruhedipurwanto <guruhedipurwanto@gmail.com>
2020-10-10 20:49:38 +0800

    create register page ui

---
+++ web/src/components/InputField.tsx
@@ -0,0 +1,32 @@
+import {
+  FormControl,
+  FormLabel,
+  Input,
+  FormErrorMessage,
+} from '@chakra-ui/core';
+import { useField } from 'formik';
+import React from 'react';
+
+type InputFieldProps = React.InputHTMLAttributes<HTMLInputElement> & {
+  name: string;
+  label: string;
+  placeholder: string;
+};
+
+const InputField: React.FC<InputFieldProps> = ({
+  label,
+  placeholder,
+  size: _,
+  ...props
+}) => {
+  const [field, { error }] = useField(props);
+  return (
+    <FormControl mt={6} isInvalid={!!error}>
+      <FormLabel htmlFor={field.name}>{label}</FormLabel>
+      <Input {...field} {...props} id={field.name} placeholder={placeholder} />
+      {error ? <FormErrorMessage>{error}</FormErrorMessage> : null}
+    </FormControl>
+  );
+};
+
+export default InputField;
---
+++ web/src/pages/register.tsx
@@ -0,0 +1,66 @@
+import React from 'react';
+import { Formik, Form } from 'formik';
+import AppBar from '../components/AppBar';
+import InputField from '../components/InputField';
+import { Button, Box, Flex } from '@chakra-ui/core';
+
+const Register: React.FC = () => {
+  return (
+    <>
+      <AppBar />
+      <Formik
+        initialValues={{
+          username: '',
+          password: '',
+          confirmpassword: '',
+          email: '',
+        }}
+        onSubmit={(values) => console.log(values)}
+      >
+        {({ isSubmitting }) => (
+          <Box w="600px" mx="auto">
+            <Form>
+              <InputField
+                name="username"
+                placeholder="Your Username"
+                label="Username"
+              />
+              <InputField
+                name="email"
+                placeholder="Your email"
+                label="Email"
+                type="email"
+              />
+              <InputField
+                name="password"
+                placeholder="password"
+                label="Password"
+                type="password"
+              />
+              <InputField
+                name="confirmpassword"
+                placeholder="Type your password again"
+                label="Confirm Password"
+                type="password"
+              />
+              <Flex justify="center">
+                <Button
+                  mt={6}
+                  type="submit"
+                  isLoading={isSubmitting}
+                  color="#f3f3f3"
+                  bg="#130487"
+                  _hover={{ backgroundColor: '#93a0c7', color: '#000' }}
+                >
+                  Register
+                </Button>
+              </Flex>
+            </Form>
+          </Box>
+        )}
+      </Formik>
+    </>
+  );
+};
+
+export default Register;
