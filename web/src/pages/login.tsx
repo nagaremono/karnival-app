b6a0deeeb99fd4077bc00a9dab2aaab4a24f09cb
guruhedipurwanto <guruhedipurwanto@gmail.com>
2020-10-11 13:28:21 +0800

    update cache on login

--- web/src/pages/login.tsx
+++ web/src/pages/login.tsx
@@ -2,7 +2,7 @@ import { Formik, Form } from 'formik';
 import AppBar from '../components/AppBar';
 import InputField from '../components/InputField';
 import { Button, Box, Flex } from '@chakra-ui/core';
-import { useLoginMutation } from '../generated/graphql';
+import { MeDocument, MeQuery, useLoginMutation } from '../generated/graphql';
 import { withApollo } from '../utils/withApollo';
 import { toErrorMap } from '../utils/toErrorMap';
 import { useRouter } from 'next/router';
@@ -19,7 +19,18 @@ const Login: React.FC = () => {
           password: '',
         }}
         onSubmit={async (values, { setErrors }) => {
-          const response = await login({ variables: values });
+          const response = await login({
+            variables: values,
+            update: (cache, { data }) => {
+              cache.writeQuery<MeQuery>({
+                query: MeDocument,
+                data: {
+                  __typename: 'Query',
+                  me: data?.login.user,
+                },
+              });
+            },
+          });
 
           if (response.data?.login.errors) {
             setErrors(toErrorMap(response.data.login.errors));
