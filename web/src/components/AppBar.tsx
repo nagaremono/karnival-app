b66172dc7ef133976c9e413e8e7eac8d83f4dfbb
guruhedipurwanto <guruhedipurwanto@gmail.com>
2020-10-11 12:53:20 +0800

    app bar displays currently logged in username

--- web/src/components/AppBar.tsx
+++ web/src/components/AppBar.tsx
@@ -1,7 +1,10 @@
 import { Flex, Heading, Link } from '@chakra-ui/core';
 import React from 'react';
+import { useMeQuery } from '../generated/graphql';
 
 const AppBar = () => {
+  const { data, loading } = useMeQuery();
+
   return (
     <header>
       <Flex justifyContent="space-between" height="100px" bg="#2b2559">
@@ -14,14 +17,25 @@ const AppBar = () => {
         >
           Karnival App
         </Heading>
-        <Flex justifyContent="space-evenly" alignItems="center" w="15%">
-          <Link color="#f3f3f3" fontSize="1.5rem">
-            Register
-          </Link>
-          <Link color="#f3f3f3" fontSize="1.5rem">
-            Login
-          </Link>
-        </Flex>
+        {!data?.me || loading ? (
+          <Flex justifyContent="space-evenly" alignItems="center" w="15%">
+            <Link color="#f3f3f3" fontSize="1.4rem">
+              Register
+            </Link>
+            <Link color="#f3f3f3" fontSize="1.4rem">
+              Login
+            </Link>
+          </Flex>
+        ) : (
+          <Flex justifyContent="space-evenly" alignItems="center" w="15%">
+            <Link color="#f3f3f3" fontSize="1.5rem">
+              {data.me.username}
+            </Link>
+            <Link color="#f3f3f3" fontSize="1.3rem">
+              Logout
+            </Link>
+          </Flex>
+        )}
       </Flex>
     </header>
   );
