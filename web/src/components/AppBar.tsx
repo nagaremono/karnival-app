458a315fa8670a1aa7ddfaa0b6d1fc74e19f2d99
guruhedipurwanto <guruhedipurwanto@gmail.com>
2020-10-13 17:26:06 +0800

    display new event page link

--- web/src/components/AppBar.tsx
+++ web/src/components/AppBar.tsx
@@ -1,4 +1,4 @@
-import { Button, Flex, Heading, Link } from '@chakra-ui/core';
+import { Button, Flex, Heading, Icon, IconButton, Link } from '@chakra-ui/core';
 import React from 'react';
 import { useLogoutMutation, useMeQuery } from '../generated/graphql';
 import NextLink from 'next/link';
@@ -37,10 +37,23 @@ const AppBar = () => {
             </NextLink>
           </Flex>
         ) : (
-          <Flex justifyContent="space-evenly" alignItems="center" w="15%">
+          <Flex justifyContent="space-evenly" alignItems="center" w="25%">
             <Link color="#f3f3f3" fontSize="1.5rem">
               {data.me.username}
             </Link>
+            <NextLink href="/new-event">
+              <Link
+                display="flex"
+                justifyContent="center"
+                alignItems="center"
+                color="#f3f3f3"
+                fontSize={'1.2rem'}
+                py={2}
+              >
+                <Icon aria-label="Post new event" mx={2} name="add" />
+                Post new event
+              </Link>
+            </NextLink>
             <Button
               onClick={async () => {
                 await logout();
