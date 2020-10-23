db5fb7ad862d06c9d03e05b3cf6527433acc60d1
guruhedipurwanto <guruhedipurwanto@gmail.com>
2020-10-23 21:14:44 +0800

    add loading indicator

--- web/src/pages/_app.tsx
+++ web/src/pages/_app.tsx
@@ -1,13 +1,28 @@
 import { ThemeProvider, CSSReset } from '@chakra-ui/core';
+import NProgress from 'nprogress';
+import Router from 'next/router';
+import Head from 'next/head';
 
 import theme from '../theme';
 
+Router.events.on('routeChangeStart', (url) => {
+  console.log(`Loading: ${url}`);
+  NProgress.start();
+});
+Router.events.on('routeChangeComplete', () => NProgress.done());
+Router.events.on('routeChangeError', () => NProgress.done());
+
 function MyApp({ Component, pageProps }: any) {
   return (
-    <ThemeProvider theme={theme}>
-      <CSSReset />
-      <Component {...pageProps} />
-    </ThemeProvider>
+    <>
+      <Head>
+        <link rel="stylesheet" type="text/css" href="/nprogress.css" />
+      </Head>
+      <ThemeProvider theme={theme}>
+        <CSSReset />
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </>
   );
 }
 
