e9af5437c82012044dd145f7c860f0c5b0247745
guruhedipurwanto <guruhedipurwanto@gmail.com>
2021-05-01 00:16:03 +0800

    shorten header title

--- web/src/components/AppBar.tsx
+++ web/src/components/AppBar.tsx
@@ -37,7 +37,7 @@ const AppBar = () => {
           alignItems="center"
           as="span"
         >
-          <NextLink href="/">Karnival App</NextLink>
+          <NextLink href="/">Karnival</NextLink>
         </Heading>
         {!data?.me || loading ? (
           <Menu>
