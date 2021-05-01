947274cb32eb4d998a6e0ebfcc4a774979002f4d
guruhedipurwanto <guruhedipurwanto@gmail.com>
2021-05-01 22:53:26 +0800

    disable database sync to be closer to prod in dev

--- server/ormconfig.js
+++ server/ormconfig.js
@@ -3,7 +3,7 @@ const isProd = process.env.NODE_ENV === 'production';
 module.exports = {
   type: 'postgres',
   url: process.env.DATABASE_URL,
-  synchronize: isProd ? false : true,
+  synchronize: false,
   entities: ['dist/entities/*.js'],
   migrations: ['dist/migrations/*.js'],
   logging: isProd ? false : true,
