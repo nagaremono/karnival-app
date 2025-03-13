0e1dbb08bdd5f632992100e372b036ac65b1866a
nagaremono <ediguruhdev@gmail.com>
2025-03-13 23:32:38 +0700

    feat(web): add event card component

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,3 +1,20 @@
+import EventCard, { EventCardProps } from '@karnival/components/event-card';
+
 export default function Home() {
-  return <main></main>;
+  const agenda: EventCardProps['agenda'] = {
+    name: 'Event A',
+    organizer: {
+      username: 'Admin',
+    },
+    description: 'An example event',
+    venue: 'A wonderful venue',
+    startTime: new Date(),
+    endTime: new Date(),
+  };
+
+  return (
+    <main>
+      <EventCard agenda={agenda} />
+    </main>
+  );
 }
---
+++ web/src/components/event-card.tsx
@@ -0,0 +1,89 @@
+import {
+  Box,
+  Heading,
+  Badge,
+  // Divider,
+  Grid,
+  Text,
+  // Link,
+  // Flex,
+} from '@chakra-ui/react';
+import React from 'react';
+// import NextLink from 'next/link';
+// import { ParticipatingStatus } from './ParticipatingStatus';
+// import EditDeleteButtons from './EditDeleteButtons';
+
+export type EventCardProps = {
+  agenda: {
+    name: string;
+    organizer: {
+      username: string;
+    };
+    description: string;
+    venue: string;
+    startTime: Date;
+    endTime: Date;
+  };
+};
+
+const EventCard = ({ agenda }: EventCardProps) => {
+  return (
+    <Box
+      borderWidth="5px"
+      borderStyle="solid"
+      borderColor="#2b2559"
+      boxShadow="md"
+      p={4}
+      color="#EFECCA"
+    >
+      {/* <NextLink */}
+      {/*   href={{ */}
+      {/*     pathname: '/event/[id]', */}
+      {/*     query: { id: agenda?.id.toString() }, */}
+      {/*   }} */}
+      {/* > */}
+      {/*   <Link> */}
+      <Heading textStyle="xl" mb={2}>
+        {agenda?.name}
+      </Heading>
+      {/*   </Link> */}
+      {/* </NextLink> */}
+
+      <Text mb={2} as="span" textStyle="l">
+        Organized by <Badge size="md">{agenda?.organizer.username}</Badge>
+      </Text>
+      {/* <EditDeleteButtons */}
+      {/*   agendaId={agenda.id} */}
+      {/*   organizerId={agenda.organizerId} */}
+      {/* /> */}
+      {/* <Divider borderWidth="3px" /> */}
+      <Grid
+        mt={4}
+        gap={2}
+        templateColumns="repeat(2, minmax(0, 1fr))"
+        textStyle="md"
+      >
+        <Text gridColumn="1 / -1">
+          {agenda?.description.slice(0, 150) +
+            (agenda.description.length > 150 ? '...' : '')}
+        </Text>
+        <Text gridColumn="1 / -1">Venue: {agenda?.venue}</Text>
+        <Text>
+          Start:{' '}
+          {new Date(agenda?.startTime).toLocaleString('id-ID', {
+            timeZone: 'Asia/Jakarta',
+          })}
+        </Text>
+        <Text>
+          End:{' '}
+          {new Date(agenda?.endTime).toLocaleString('id-ID', {
+            timeZone: 'Asia/Jakarta',
+          })}
+        </Text>
+      </Grid>
+      {/* <ParticipatingStatus agenda={agenda} /> */}
+    </Box>
+  );
+};
+
+export default EventCard;
