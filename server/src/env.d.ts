1361068ccd409039899fa497a48da67408514723
nagaremono <ediguruhdev@gmail.com>
2025-06-08 18:49:17 +0700

    chore(server): update .env file

--- server/src/env.d.ts
+++ server/src/env.d.ts
@@ -1,8 +1,13 @@
-declare namespace NodeJS {
-  interface ProcessEnv {
-    DATABASE_URL: string;
-    PORT: string;
-    ORIGIN: string;
-    SERVER_BASE_URL: string;
+declare global {
+  namespace NodeJS {
+    interface ProcessEnv {
+      DATABASE_URL: string;
+      PORT: string;
+      ORIGIN: string;
+      BETTER_AUTH_SECRET: string;
+      BETTER_AUTH_URL: string;
+    }
   }
 }
+
+export {}
